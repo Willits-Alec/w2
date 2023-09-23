dd505634cae0c7ee7304be03558c60e1dc8319f2
alec willits <alecwillits@gmail.com>
2023-09-23 13:43:52 -0600

    removed body parser

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
+
 app.use('/', require('./routes/index'))
 
 mongodb.initDb((err, mongodb) =>{
