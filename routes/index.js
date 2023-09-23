f497e682f8f83923c0b41d95b7fe832da5c7bbb7
alec willits <alecwillits@gmail.com>
2023-09-23 13:25:36 -0600

    removed comment and added procfile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const mainController = require('../controllers/index');
 
 const router = express.Router();
 
-// routes.get('/', mainController.contacts);
 router.get('/', mainController.getContacts);
 
 router.use('/contacts', require('./contacts'))
